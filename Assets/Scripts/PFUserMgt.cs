05795e791016b3afb8bb736e2d04131a2378f476
notJY <123806420+notJY@users.noreply.github.com>
2023-11-27 17:23:13 +0800

    project

---
+++ Assets/Scripts/PFUserMgt.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PFUserMgt : MonoBehaviour
+{
+    [SerializeField] TMP_Text msgBox;
+    [SerializeField] TMP_InputField if_username, if_password, if_email;
+
+    private string playfabID, displayName;
+
+    public void OnButtonRegUser()
+    {
+        var regReq = new RegisterPlayFabUserRequest
+        {
+            Email = if_email.text,
+            Password = if_password.text,
+            Username = if_username.text,
+            DisplayName = if_username.text
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(regReq, OnRegSucc, OnError);
+    }
+
+    void OnRegSucc(RegisterPlayFabUserResult r)
+    {
+        msgBox.text = "Success! " + r.PlayFabId;
+    }
+
+    void OnError(PlayFabError e)
+    {
+        msgBox.text = "Error " + e.GenerateErrorReport();
+    }
+
+    public void OnButtonLogin()
+    {
+        Debug.Log(if_username.text);
+        if (if_username.text != "")
+        {
+            var loginReq = new LoginWithPlayFabRequest
+            {
+                Username = if_username.text,
+                Password = if_password.text
+            };
+            PlayFabClientAPI.LoginWithPlayFab(loginReq, OnLoginSucc, OnError);
+        }
+        else
+        {
+            var loginReq = new LoginWithEmailAddressRequest
+            {
+                Email = if_email.text,
+                Password = if_password.text
+            };
+            PlayFabClientAPI.LoginWithEmailAddress(loginReq, OnLoginSucc, OnError);
+        }
+    }
+
+    string GetPlayerID()
+    {
+        var accReq = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(accReq, r => { playfabID = r.AccountInfo.PlayFabId; }, OnError);
+        return playfabID;
+    }
+
+    string GetPlayerDisplayName()
+    {
+
+        var profileReq = new GetPlayerProfileRequest
+        {
+            PlayFabId = GetPlayerID()
+        };
+        PlayFabClientAPI.GetPlayerProfile(profileReq, r => { displayName = r.PlayerProfile.DisplayName; }, OnError);
+        
+        return displayName;
+    }
+
+    void updateDisplayName()
+    {
+        var updateNameReq = new UpdateUserTitleDisplayNameRequest
+        {
+            DisplayName = "Guest"
+        };
+        PlayFabClientAPI.UpdateUserTitleDisplayName(updateNameReq, OnDisplayNameUpdate, OnError);
+    }
+
+    void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult r)
+    {
+        Debug.Log("Display name updated: " + r.DisplayName);
+    }
+
+    void OnLoginSucc(LoginResult r)
+    {
+        if (GetPlayerDisplayName() == null)
+        {
+            updateDisplayName();
+        }
+
+        msgBox.text = "Success " + r.PlayFabId;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void OnResetPassword()
+    { 
+        var resetReq = new SendAccountRecoveryEmailRequest
+        {
+            Email = if_email.text,
+            TitleId = PlayFabSettings.TitleId
+        };
+        PlayFabClientAPI.SendAccountRecoveryEmail(resetReq, OnResetSucc, OnError);
+    }
+
+    void OnResetSucc(SendAccountRecoveryEmailResult r)
+    {
+        msgBox.text = "Email sent ";
+    }
+
+    public void OnGuestLogin()
+    {
+        var guestReq = new LoginWithCustomIDRequest
+        {
+            CreateAccount = true,
+            CustomId = PlayFabSettings.DeviceUniqueIdentifier
+        };
+        PlayFabClientAPI.LoginWithCustomID(guestReq, OnLoginSucc, OnError);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] TMP_Text highscoreTxt, currencyTxt;
+    [HideInInspector] public int highscore, currency;
+
+    [SerializeField] TMP_Text inGameMsg;
+    public bool isMenu;
+
+    private void Awake()
+    {
+        GetHighscore();
+        if (isMenu)
+        {
+            GetCurrency();
+        }
+    }
+
+    public void OnBackButton(GameObject currentUI)
+    {
+        currentUI.SetActive(false);
+        mainMenu.SetActive(true);
+        GetHighscore();
+    }
+
+    public void OnUIButton(GameObject newUI)
+    {
+        mainMenu.SetActive(false);
+        newUI.SetActive(true);
+    }
+
+    void OnError(PlayFabError e)
+    {
+        highscoreTxt.text = "Error: " + e.GenerateErrorReport();
+    }
+
+    void GetHighscore()
+    {
+        var statsReq = new GetPlayerStatisticsRequest()
+        {
+            StatisticNames = new List<string> ()
+            {
+                "Highscore"
+            }
+        };
+        PlayFabClientAPI.GetPlayerStatistics(statsReq, OnHighscoreReceived, OnError);
+    }
+
+    void OnHighscoreReceived(GetPlayerStatisticsResult r)
+    {
+        if (!isMenu && (r.Statistics != null))
+        {
+            highscore = r.Statistics[0].Value;
+        }
+        else if (r.Statistics != null)
+        {
+            highscoreTxt.text = "Highscore: \n" + r.Statistics[0].Value;
+            highscore = r.Statistics[0].Value;
+        }
+        else
+        {
+            highscoreTxt.text = "Highscore: \nError";
+            highscore = 0;
+        }
+    }
+
+    public void GetCurrency()
+    {
+        PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(), OnInventoryReceived, OnError);
+    }
+
+    void OnInventoryReceived(GetUserInventoryResult r)
+    {
+        currency = r.VirtualCurrency["GD"];
+        currencyTxt.text = currency.ToString();
+    }
+
+    public void AddCurrency(int amount)
+    {
+        var addCurrencyReq = new AddUserVirtualCurrencyRequest()
+        {
+            VirtualCurrency = "GD",
+            Amount = amount
+        };
+        PlayFabClientAPI.AddUserVirtualCurrency(addCurrencyReq, OnCurrencyModified, OnCurrencyError);
+    }
+
+    void OnCurrencyError(PlayFabError e)
+    {
+        inGameMsg.text = "Error: " + e.GenerateErrorReport();
+    }
+
+    void OnCurrencyModified(ModifyUserVirtualCurrencyResult r)
+    {
+        currency = r.Balance;
+        inGameMsg.text = "Gold earned: " + r.BalanceChange.ToString();
+    }
+}
